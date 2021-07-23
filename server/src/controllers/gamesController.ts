4d25ae1dcecd1c9210acd53e3e705d5ad114b30d
Angel Rivilla <angelrivillaarre@gmail.com>
2021-07-23 23:41:44 +0200

    commit

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -3,9 +3,35 @@ import pool from '../database';
 
 class GamesController{
 
-    public index (req: Request, res: Response){
-        pool.query('DESCRIBE game');
-        res.json('games');
+    public async list (req: Request, res: Response){
+        const games = await pool.query('SELECT * FROM game');
+        res.json(games);
+    }
+
+    public async getOne (req: Request, res: Response): Promise<any>{
+        const { id } = req.params;
+        const games = await pool.query('SELECT * FROM game WHERE id = ?', [id]);
+        if(games.length > 0){
+            return res.json(games[0]);
+        }
+        res.status(404).json({text: "The game doesn't exist"})
+    }
+    
+    public async create (req: Request, res: Response): Promise<void>{
+        await pool.query('INSERT INTO game set ?', [req.body]);
+        res.json({message: 'Game Saved'});
+    }
+
+    public async update (req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('UPDATE game set ? WHERE id = ?', [req.body, id]);
+        res.json({message: 'The game was updated'});
+    }
+
+    public async delete (req: Request, res: Response): Promise<void>{
+        const { id } = req.params;
+        await pool.query('DELETE FROM game WHERE id = ?', [id]);
+        res.json({message: 'The game was deleted'});
     }
 }
 
