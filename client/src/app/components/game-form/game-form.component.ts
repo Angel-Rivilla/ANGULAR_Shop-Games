03581ebbd205f90d70000f9989397c6ece4e7791
Angel Rivilla <angelrivillaarre@gmail.com>
2021-07-24 15:22:03 +0200

    commit

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Game } from 'src/app/models/Game';
 
 import { GamesService } from '../../services/games.service';
@@ -22,9 +22,22 @@ export class GameFormComponent implements OnInit {
     created_at: new Date()
   };
 
-  constructor(private gamesService: GamesService, private router: Router) { }
+  edit: boolean = false;
+
+  constructor(private gamesService: GamesService, private router: Router, private activedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const params = this.activedRoute.snapshot.params;
+    if(params.id){
+      this.gamesService.getGame(params.id).
+        subscribe(
+          res => {
+            this.game = res;
+            this.edit = true;
+          },
+          err => console.error(err)
+        )
+    }
   }
 
   saveNewGame(){
@@ -39,5 +52,19 @@ export class GameFormComponent implements OnInit {
         },
         err => console.error(err)
       );
+  }
+
+  updateGame(){
+    delete this.game.created_at;
+    if(this.game.id != undefined){
+      this.gamesService.updateGame(this.game.id, this.game)
+        .subscribe(
+          res => {
+            console.log(res);
+            this.router.navigate(['/games']);
+          },
+          err => console.error(err)
+        )
+    }
   }
 }
