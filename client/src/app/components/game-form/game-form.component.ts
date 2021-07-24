5abf68075351c30a9526b6f6dd209f60892bc269
Angel Rivilla <angelrivillaarre@gmail.com>
2021-07-24 14:16:09 +0200

    commit

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Game } from 'src/app/models/Game';
+
+import { GamesService } from '../../services/games.service';
+
 
 @Component({
   selector: 'app-game-form',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameFormComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') classes = 'row';
+  
+  game: Game = {
+    id: 0,
+    title: '',
+    description: '',
+    image: '',
+    created_at: new Date()
+  };
+
+  constructor(private gamesService: GamesService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveNewGame(){
+    delete this.game.created_at;
+    delete this.game.id;
+    
+    this.gamesService.saveGame(this.game)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/games']);
+        },
+        err => console.error(err)
+      );
+  }
 }
--- client/src/app/components/game-list/game-list.component.ts
+++ client/src/app/components/game-list/game-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 import { GamesService } from '../../services/games.service';
 
@@ -9,11 +9,27 @@ import { GamesService } from '../../services/games.service';
 })
 export class GameListComponent implements OnInit {
 
+  @HostBinding('class') classes = 'row';
+
   games: any = [];
 
   constructor(private gamesService: GamesService) { }
 
   ngOnInit(): void {
+    this.getGames();
+  }
+
+  deleteGame(id: string){
+    this.gamesService.deleteGame(id).subscribe(
+      res => {
+        console.log(res);
+        this.getGames();
+      },
+      err => console.log(err)
+    );
+  }
+
+  getGames(){
     this.gamesService.getGames().subscribe(
       res => {
         this.games = res;
